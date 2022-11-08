c324eb57e90f570117939b2ae9d623e09315c41d
Deucalio <hjamad89@gmail.com>
2022-11-08 21:03:52 +0500

    add state to disable cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Main from "./components/Main";
 import Cards from "./components/Cards";
+// import Test from "./components/Test"
 
 const App = () => {
   return (
     <div className="App">
       <Main />
       <Cards/>
+
+      {/* <Test /> */}
     </div>
   );
 };
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,30 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Cards = () => {
-  const [clickedCards, addClickedCards] = useState([]);
+  const [clickedCards, setClickedCards] = useState([]);
+  const [cardDisabled, setCardDisabled] = useState(false)
 
-  const matchCard = () => {
-    const [card1, card2] = clickedCards;
-    if (clickedCards.length === 2) {
-      console.log(card1, "\n", card2);
-    } else {
-      return "no card";
-    }
-  };
+  useEffect(() => {
+    const matchCard = () => {
+      const [card1, card2] = clickedCards;
+      if (clickedCards.length >= 2) {
+        setCardDisabled(true)
+        console.log(card1, "\n", card2);
+      } else {
+        return "no card";
+      }
+    };
+    matchCard();
+  }, [clickedCards]);
 
   const flipCard = (clickedCard) => {
     const [frontCard, backCard] = clickedCard.children;
     frontCard.style.transform = "rotateY(180deg)";
     backCard.style.transform = "rotate(0)";
 
-    addClickedCards(prevCards => ([...prevCards, clickedCard]));
-    matchCard();
+    setClickedCards((prevCards) => [...prevCards, clickedCard]);
 
     setTimeout(() => {
       frontCard.style.transform = "";
       backCard.style.transform = "";
-      addClickedCards([]);
-    }, 1500);
+      setClickedCards([]);
+      setCardDisabled(false)
+    }, 2000);
   };
 
   const handleClick = (e) => {
@@ -35,57 +40,28 @@ const Cards = () => {
     <div className="card-wrapper xl:x-gap-1 container mx-auto mt-5 grid grid-cols-4 gap-2 px-4 pb-4 md:w-5/6 lg:gap-4 lg:px-36">
       <div
         onClick={handleClick}
-        className="card h-16 sm:h-28 xl:w-44 cursor-pointer"
+        className={`card h-16 sm:h-28 xl:w-44 cursor-pointer ${cardDisabled ? "pointer-events-none" : ""}`}
       >
         <div className="card__side card__side--front card__side--front-1 h-full rounded-sm bg-cover bg-center"></div>
         <div className="card__side card__side--back card__side--back-1 h-full rounded-sm bg-cover bg-center"></div>
       </div>
       <div
         onClick={handleClick}
-        className="card h-16 sm:h-28 xl:w-44 cursor-pointer"
+        className={`card h-16 sm:h-28 xl:w-44 cursor-pointer ${cardDisabled ? "pointer-events-none" : ""}`}
       >
         <div className="card__side card__side--front card__side--front-1 h-full rounded-sm bg-cover bg-center"></div>
         <div className="card__side card__side--back card__side--back-1 h-full rounded-sm bg-cover bg-center"></div>
       </div>
       <div
         onClick={handleClick}
-        className="card h-16 sm:h-28 xl:w-44 cursor-pointer"
+        className={`card h-16 sm:h-28 xl:w-44 cursor-pointer ${cardDisabled ? "pointer-events-none" : ""}`}
       >
         <div className="card__side card__side--front card__side--front-1 h-full rounded-sm bg-cover bg-center"></div>
         <div className="card__side card__side--back card__side--back-1 h-full rounded-sm bg-cover bg-center"></div>
       </div>
       <div
         onClick={handleClick}
-        className="card h-16 sm:h-28 xl:w-44 cursor-pointer"
-      >
-        <div className="card__side card__side--front card__side--front-1 h-full rounded-sm bg-cover bg-center"></div>
-        <div className="card__side card__side--back card__side--back-1 h-full rounded-sm bg-cover bg-center"></div>
-      </div>
-
-      <div
-        onClick={handleClick}
-        className="card h-16 sm:h-28 xl:w-44 cursor-pointer"
-      >
-        <div className="card__side card__side--front card__side--front-1 h-full rounded-sm bg-cover bg-center"></div>
-        <div className="card__side card__side--back card__side--back-1 h-full rounded-sm bg-cover bg-center"></div>
-      </div>
-      <div
-        onClick={handleClick}
-        className="card h-16 sm:h-28 xl:w-44 cursor-pointer"
-      >
-        <div className="card__side card__side--front card__side--front-1 h-full rounded-sm bg-cover bg-center"></div>
-        <div className="card__side card__side--back card__side--back-1 h-full rounded-sm bg-cover bg-center"></div>
-      </div>
-      <div
-        onClick={handleClick}
-        className="card h-16 sm:h-28 xl:w-44 cursor-pointer"
-      >
-        <div className="card__side card__side--front card__side--front-1 h-full rounded-sm bg-cover bg-center"></div>
-        <div className="card__side card__side--back card__side--back-1 h-full rounded-sm bg-cover bg-center"></div>
-      </div>
-      <div
-        onClick={handleClick}
-        className="card h-16 sm:h-28 xl:w-44 cursor-pointer"
+        className={`card h-16 sm:h-28 xl:w-44 cursor-pointer ${cardDisabled ? "pointer-events-none" : ""}`}
       >
         <div className="card__side card__side--front card__side--front-1 h-full rounded-sm bg-cover bg-center"></div>
         <div className="card__side card__side--back card__side--back-1 h-full rounded-sm bg-cover bg-center"></div>
@@ -93,28 +69,28 @@ const Cards = () => {
 
       <div
         onClick={handleClick}
-        className="card h-16 sm:h-28 xl:w-44 cursor-pointer"
+        className={`card h-16 sm:h-28 xl:w-44 cursor-pointer ${cardDisabled ? "pointer-events-none" : ""}`}
       >
         <div className="card__side card__side--front card__side--front-1 h-full rounded-sm bg-cover bg-center"></div>
         <div className="card__side card__side--back card__side--back-1 h-full rounded-sm bg-cover bg-center"></div>
       </div>
       <div
         onClick={handleClick}
-        className="card h-16 sm:h-28 xl:w-44 cursor-pointer"
+        className={`card h-16 sm:h-28 xl:w-44 cursor-pointer ${cardDisabled ? "pointer-events-none" : ""}`}
       >
         <div className="card__side card__side--front card__side--front-1 h-full rounded-sm bg-cover bg-center"></div>
         <div className="card__side card__side--back card__side--back-1 h-full rounded-sm bg-cover bg-center"></div>
       </div>
       <div
         onClick={handleClick}
-        className="card h-16 sm:h-28 xl:w-44 cursor-pointer"
+        className={`card h-16 sm:h-28 xl:w-44 cursor-pointer ${cardDisabled ? "pointer-events-none" : ""}`}
       >
         <div className="card__side card__side--front card__side--front-1 h-full rounded-sm bg-cover bg-center"></div>
         <div className="card__side card__side--back card__side--back-1 h-full rounded-sm bg-cover bg-center"></div>
       </div>
       <div
         onClick={handleClick}
-        className="card h-16 sm:h-28 xl:w-44 cursor-pointer"
+        className={`card h-16 sm:h-28 xl:w-44 cursor-pointer ${cardDisabled ? "pointer-events-none" : ""}`}
       >
         <div className="card__side card__side--front card__side--front-1 h-full rounded-sm bg-cover bg-center"></div>
         <div className="card__side card__side--back card__side--back-1 h-full rounded-sm bg-cover bg-center"></div>
@@ -122,28 +98,57 @@ const Cards = () => {
 
       <div
         onClick={handleClick}
-        className="card h-16 sm:h-28 xl:w-44 cursor-pointer"
+        className={`card h-16 sm:h-28 xl:w-44 cursor-pointer ${cardDisabled ? "pointer-events-none" : ""}`}
       >
         <div className="card__side card__side--front card__side--front-1 h-full rounded-sm bg-cover bg-center"></div>
         <div className="card__side card__side--back card__side--back-1 h-full rounded-sm bg-cover bg-center"></div>
       </div>
       <div
         onClick={handleClick}
-        className="card h-16 sm:h-28 xl:w-44 cursor-pointer"
+        className={`card h-16 sm:h-28 xl:w-44 cursor-pointer ${cardDisabled ? "pointer-events-none" : ""}`}
       >
         <div className="card__side card__side--front card__side--front-1 h-full rounded-sm bg-cover bg-center"></div>
         <div className="card__side card__side--back card__side--back-1 h-full rounded-sm bg-cover bg-center"></div>
       </div>
       <div
         onClick={handleClick}
-        className="card h-16 sm:h-28 xl:w-44 cursor-pointer"
+        className={`card h-16 sm:h-28 xl:w-44 cursor-pointer ${cardDisabled ? "pointer-events-none" : ""}`}
       >
         <div className="card__side card__side--front card__side--front-1 h-full rounded-sm bg-cover bg-center"></div>
         <div className="card__side card__side--back card__side--back-1 h-full rounded-sm bg-cover bg-center"></div>
       </div>
       <div
         onClick={handleClick}
-        className="card h-16 sm:h-28 xl:w-44 cursor-pointer"
+        className={`card h-16 sm:h-28 xl:w-44 cursor-pointer ${cardDisabled ? "pointer-events-none" : ""}`}
+      >
+        <div className="card__side card__side--front card__side--front-1 h-full rounded-sm bg-cover bg-center"></div>
+        <div className="card__side card__side--back card__side--back-1 h-full rounded-sm bg-cover bg-center"></div>
+      </div>
+
+      <div
+        onClick={handleClick}
+        className={`card h-16 sm:h-28 xl:w-44 cursor-pointer ${cardDisabled ? "pointer-events-none" : ""}`}
+      >
+        <div className="card__side card__side--front card__side--front-1 h-full rounded-sm bg-cover bg-center"></div>
+        <div className="card__side card__side--back card__side--back-1 h-full rounded-sm bg-cover bg-center"></div>
+      </div>
+      <div
+        onClick={handleClick}
+        className={`card h-16 sm:h-28 xl:w-44 cursor-pointer ${cardDisabled ? "pointer-events-none" : ""}`}
+      >
+        <div className="card__side card__side--front card__side--front-1 h-full rounded-sm bg-cover bg-center"></div>
+        <div className="card__side card__side--back card__side--back-1 h-full rounded-sm bg-cover bg-center"></div>
+      </div>
+      <div
+        onClick={handleClick}
+        className={`card h-16 sm:h-28 xl:w-44 cursor-pointer ${cardDisabled ? "pointer-events-none" : ""}`}
+      >
+        <div className="card__side card__side--front card__side--front-1 h-full rounded-sm bg-cover bg-center"></div>
+        <div className="card__side card__side--back card__side--back-1 h-full rounded-sm bg-cover bg-center"></div>
+      </div>
+      <div
+        onClick={handleClick}
+        className={`card h-16 sm:h-28 xl:w-44 cursor-pointer ${cardDisabled ? "pointer-events-none" : ""}`}
       >
         <div className="card__side card__side--front card__side--front-1 h-full rounded-sm bg-cover bg-center"></div>
         <div className="card__side card__side--back card__side--back-1 h-full rounded-sm bg-cover bg-center"></div>
